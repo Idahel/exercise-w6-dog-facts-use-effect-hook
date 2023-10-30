f3b2eeefc763a4326cf924bcf9429396b5952742
Idahel <idakatrinhellgren@gmail.com>
2023-10-30 15:35:19 +0100

    Exercise useEffect API

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,40 @@
-// Import required  hooks
+import React, { useState, useEffect } from "react";
 import { DogFact } from "./components/DogFact";
 
 export const App = () => {
-  // Hint: Initialize state for storing the dog fact
-  // Hint: Define the API endpoint
-  // Hint: Create a function to fetch the dog fact
-  // Hint: Use the useEffect hook to fetch the dog fact when the component mounts
+  const [dogFact, setDogFact] = useState(null);
+
+  useEffect(() => {
+    // Fetch a dog fact when the component mounts
+    fetch("https://dogapi.dog/api/v2/facts")
+      .then((res) => res.json())
+      .then((json) => {
+        // Set the first dog fact from the API response
+        if (json.data && json.data.length > 0) {
+          setDogFact(json.data[0].attributes.body);
+        }
+      });
+  }, []);
+
+  // Function to fetch a new dog fact
+  const getNewDogFact = () => {
+    fetch("https://dogapi.dog/api/v2/facts")
+      .then((res) => res.json())
+      .then((json) => {
+        // Set a new dog fact from the API response
+        if (json.data && json.data.length > 0) {
+          setDogFact(json.data[0].attributes.body);
+        }
+      });
+  };
 
   return (
     <div className="App">
-      <DogFact />
+      {/* Display the dog fact if available, otherwise show a loading message */}
+      <DogFact body={dogFact} />
+
+      {/* Button to fetch a new dog fact */}
+      <button className = "dog-button" onClick={getNewDogFact}>Give me a dog fact</button>
     </div>
   );
 };
--- src/components/DogFact.jsx
+++ src/components/DogFact.jsx
@@ -1,11 +1,4 @@
-// DogFact Component
-export const DogFact = () => {
-  // Hint: Extract the 'body' from 'attributes' of the 'fact' prop. We'll call this 'cleanFact'.
-  // Cleaning our data means choosing and keeping a somewhat small fraction of the data, the most useful data.
-
-  // Hint: Render the 'cleanFact' if available, otherwise show a loading message
-  return <div>dog fact here.</div>;
+export const DogFact = (props) => {
+  // Render the 'body' if available, otherwise show a loading message
+  return <div className="fact">{props.body || "Loading..."}</div>;
 };
-
-// Hint: To use this component, import it in your main App component and pass the 'fact' prop to it.
-// Example: <DogFact fact={yourFactData} />
